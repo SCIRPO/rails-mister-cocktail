987e031a115667bb8544e7a78992c36b9bd673e1
sebastien scirpo <sebastien.scirpo@gmail.com>
2020-12-11 10:46:58 +0100

    creating models and validations

---
+++ db/migrate/20201211085516_create_doses.rb
@@ -0,0 +1,12 @@
+class CreateDoses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :doses do |t|
+      t.references :cocktail, null: false, foreign_key: true
+      t.references :ingredient, null: false, foreign_key: true
+      t.string :description
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
